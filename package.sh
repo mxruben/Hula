06581268f067228e663f5c17a722bae4ba1b1b72
Maxwell Ruben <maxruben1@outlook.com>
2024-03-05 00:49:15 -0500

    Fix packaging

--- package.sh
+++ package.sh
@@ -2,9 +2,11 @@ darklua process lib package/roblox/lib -c roblox.darklua.json
 darklua minify package/roblox/lib package/roblox/lib
 darklua process package/roblox/lib package/roblox/lib -c copyright.darklua.json
 mv package/roblox/lib/Hula.luau package/roblox/lib/init.luau
+sed -i -e 's/.Parent//g' package/roblox/lib/init.luau
 cp wally.toml package/roblox/wally.toml
 cp LICENSE package/roblox/LICENSE
 cp README.md package/roblox/README.md
+cp default.project.json package/roblox/default.project.json
 
 darklua minify lib package/luau/lib
 darklua process package/luau/lib package/luau/lib -c copyright.darklua.json
