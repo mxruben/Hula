7ae3c1e6883a1e8e34c2e83a84b2cf76939a66bf
Maxwell Ruben <maxruben1@outlook.com>
2024-04-14 14:24:19 -0400

    Minifying was a bad idea

--- package.sh
+++ package.sh
@@ -1,5 +1,5 @@
+# roblox luau
 darklua process lib package/roblox/lib -c roblox.darklua.json
-darklua minify package/roblox/lib package/roblox/lib
 darklua process package/roblox/lib package/roblox/lib -c copyright.darklua.json
 mv package/roblox/lib/Hula.luau package/roblox/lib/init.luau
 sed -i -e 's/.Parent//g' package/roblox/lib/init.luau
@@ -8,7 +8,7 @@ cp LICENSE package/roblox/LICENSE
 cp README.md package/roblox/README.md
 cp default.project.json package/roblox/default.project.json
 
-darklua minify lib package/luau/lib
-darklua process package/luau/lib package/luau/lib -c copyright.darklua.json
+# vanilla luau
+darklua process lib package/luau/lib -c copyright.darklua.json
 cp LICENSE package/luau/LICENSE
 cp README.md package/luau/README.md
